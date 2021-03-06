479b420b12b50c986ffa4d1d15ff4f15ca6d828b
Himanshu Sanecha <52252631+himanshusanecha@users.noreply.github.com>
2021-03-06 14:17:55 +0530

    Add files via upload

---
+++ Light.js
@@ -0,0 +1,16 @@
+function fun(){
+    var x = document.getElementsByClassName("over-img")[0];
+    var y = document.getElementsByClassName("over-img2")[0];
+    var z = document.getElementsByClassName("over-img3")[0];
+    var a = document.getElementsByClassName("over-img4")[0];
+    var b = document.getElementById("img7");
+    //console.log(x.style.display); 
+      x.style.display = "block";
+      y.style.display = "block";
+      a.style.display = "block";
+      z.style.display = "block";
+      b.style.display = "block";
+      var element = document.getElementById("mydiv");
+  element.classList.add("pyro");
+}
+
