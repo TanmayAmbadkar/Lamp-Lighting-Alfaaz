15631626dfd77f0e2063d116cbf65a7d945317cb
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2021-03-07 20:46:27 +0530

    Added API calls, removed button and added folder for director

--- Light.js
+++ Light.js
@@ -1,4 +1,4 @@
-function funh(){
+function myFunction(){
   var x = document.getElementsByClassName("over-img")[0];
   var y = document.getElementsByClassName("over-img2")[0];
   var z = document.getElementsByClassName("over-img3")[0];
@@ -17,3 +17,15 @@ function funh(){
     },5000)
 
 }
+
+function myFunc() {
+  setInterval(function(){fetch('https://ctf-iiitv.tk/lamp').then(function(response){
+  return response.json();
+ }).then(function(jsondata){
+   if(jsondata['out']==1)
+     myFunction();
+   else
+     console.log(jsondata['out']);
+ });}, 1000);
+}
+myFunc();
